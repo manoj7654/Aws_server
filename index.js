11255ca1c8cc65204a55c5fa095a2a3e2fc6fdc2
Manoj Kumar <manojsfstm5@gmail.com>
2023-03-19 07:38:08 +0530

    aws

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require("express");
+
+const app = express()
+
+app.use("/home",(req,res)=>{
+    res.send("Welcome to the server 1")
+})
+
+app.listen(4500,()=>{
+    console.log("Server is listening on port no 4500")
+})
