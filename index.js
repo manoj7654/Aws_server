453224cab34457ad33e7a8af400b792754557511
Manoj Kumar <manojsfstm5@gmail.com>
2023-03-19 07:54:22 +0530

    changes done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require("express");
 const app = express()
 
 app.use("/home",(req,res)=>{
-    res.send("Welcome to the server 1")
+    res.send("Welcome to the server 2")
 })
 
 app.listen(4500,()=>{
